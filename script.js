6558f3edbc081eb435f787882dfdd2795738cf6a
Fikatu <madalins2s@gmail.com>
2022-02-04 03:37:19 +0200

    fgdf

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const frica = document.getElementById("frica");
 const nut = document.getElementById("nuLua");
 const iati = document.getElementById("ia-ti")
 const platesc = document.getElementById("platesc");
-const plateste = document.getElementById("plateste")
+const plateste = document.getElementById("plateste");
+const betiv = document.getElementById("betiv");
+const betivo = document.getElementById("betivo");
+const ciucci = document.getElementById("ciucci");
+const aschwa = document.getElementById("aschw");
+const bine = document.getElementById("binie");
+const nesim = document.getElementById("nesimtire");
 
 function alerta() {
     alert("Bine ca iti ceri scuze. Incerc sa il calmez!");
@@ -29,9 +35,39 @@ function plate() {
     alert("He'll pay 🤮🤮🤮🤮")
 }
 
+function betivb() {
+    alert("Este un betiv...")
+}
+
+function betivos() {
+    alert("Cel mai mare betiv ordinar!")
+}
+
+function ciucciii() {
+    alert("LUTZCA!")
+}
+
+function aschwaganda() {
+    alert("Este ceaiul care ma linisteste contra acestor infecti!")
+}
+
+function biniee() {
+    alert("Acuma face oferta la unu!")
+}
+
+function nesimtiree() {
+    alert("Iar s-au scumpit...Asta-i nesimtire ma!")
+}
+
 scuze.addEventListener("click", alerta);
 frica.addEventListener("click", frica1);
 nut.addEventListener("click", nuti);
 iati.addEventListener("click", ial);
 platesc.addEventListener("click", plat);
 plateste.addEventListener("click", plate);
+betiv.addEventListener("click", betivb);
+betivo.addEventListener("click", betivos);
+ciucci.addEventListener("click", ciucciii);
+aschwa.addEventListener("click", aschwaganda);
+bine.addEventListener("click", biniee);
+nesim.addEventListener("click", nesimtiree);
