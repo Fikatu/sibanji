252b1fdbd77ab617b8f0a96b2496a265050625b6
Fikatu <madalins2s@gmail.com>
2022-02-04 03:19:58 +0200

    ttwer

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const scuze = document.getElementById("scuze");
 const frica = document.getElementById("frica");
 const nut = document.getElementById("nuLua");
 const iati = document.getElementById("ia-ti")
+const platesc = document.getElementById("platesc");
+const plateste = document.getElementById("plateste")
 
 function alerta() {
     alert("Bine ca iti ceri scuze. Incerc sa il calmez!");
@@ -19,7 +21,17 @@ function ial() {
     alert("Isi ia da-l drecu")
 }
 
+function plat() {
+    alert("Plateste el 🤮")
+}
+
+function plate() {
+    alert("He'll pay 🤮🤮🤮🤮")
+}
+
 scuze.addEventListener("click", alerta);
 frica.addEventListener("click", frica1);
 nut.addEventListener("click", nuti);
-iati.addEventListener("click", ial)
+iati.addEventListener("click", ial);
+platesc.addEventListener("click", plat);
+plateste.addEventListener("click", plate);
